7ca95894e721ce4c5ffa6150412d5bdab46c04da
SinghAvi <avinash2014mca@gmail.com>
2019-06-23 17:00:09 +0530

    basic setup for rspec

---
+++ spec/models/staff_sppec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe Staff do
+  it { should have_db_column(:email).of_type(:string) }
+  it { should have_db_column(:name).of_type(:string) }
+  it { should have_db_column(:created_at).of_type(:datetime) }
+  it { should have_db_column(:updated_at).of_type(:datetime) }
+
+  it { should have_many :bookings }
+end
